c89cd093573026869dde46b5bc82ea371740e987
Scott Fasone <scott181182@yahoo.com>
2018-08-16 10:32:41 -0400

    Administrative corrections

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 [![MIT license](http://img.shields.io/badge/license-MIT-brightgreen.svg)](http://opensource.org/licenses/MIT)
 
-# Botiful
+# Galactica Bot
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { DiscordBot } from "botiful";
 import { TextChannel } from "discord.js";
 
-(function main()
+(async function main()
 {
     const bot = new DiscordBot("private/config.json");
 
@@ -16,8 +16,5 @@ import { TextChannel } from "discord.js";
     bot.log.info(`${__dirname}/actions`);
     bot.load_actions(`${__dirname}/actions`);
 
-    bot.start().then(() => {
-        const dev_chan = bot.client.guilds.first().channels
-            .find(chan => chan.name === "developers") as TextChannel;
-    });
+    await bot.start();
 })();
